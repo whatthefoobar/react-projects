2330a67b76968a14eb5500f9e1b7044d38da493b
whatthefoobar <irina.mntn@gmail.com>
2021-12-05 15:38:52 +0100

    reviews done

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -5,10 +5,40 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] =useState(0);
   // console.log(people);
-  const {name, job, image, text}= people[index];
+  const {name,job, image, text} = people[index];
+
+  const checkNumber =(number) =>{
+    if(number > people.length - 1){
+      return 0;
+    }
+    if(number < 0){
+      return people.length - 1;
+    }
+    return number;
+  }
+
+  const nextPerson =()=>{
+    setIndex((index)=>{
+      let newIndex = index +1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson =()=>{
+    setIndex((index)=>{
+      let newIndex = index -1;
+      return checkNumber(newIndex);
+    })
+  }
+
+  const  randomPerson=()=>{
+   let randomNumber = Math.floor(Math.random()* people.length);
+   console.log(randomNumber);
+   return setIndex(randomNumber);
+  }
+
   return <article className="review">
     <div className="img-container">
-      <img src={image} alt={name}className="person-img"/>
+      <img src={image} alt={name} className="person-img"/>
       <span className="quote-icon">
         <FaQuoteRight/>
       </span>
@@ -17,16 +47,16 @@ const Review = () => {
     <p className="job">{job}</p>
     <p className="info">{text}</p>
     <div className="button-container">
-      <button className="prev-btn">
+      <button className="prev-btn" onClick={prevPerson}>
         <FaChevronLeft/>
       </button>
-      <button className="next-btn">
+      <button className="next-btn" onClick={nextPerson}>
         <FaChevronRight/>
       </button >
     </div>
-    <button className="random-btn">
+    <button className="random-btn" onClick={randomPerson}>
         surprise me
-      </button >
+    </button > {/*onClick={randomPerson} */}
   </article>;
 };
 
