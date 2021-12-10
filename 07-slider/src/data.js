cf9b87457133e5b19bd22dfb8453ade92d459139
whatthefoobar <irina.mntn@gmail.com>
2021-12-10 17:55:00 +0100

    boilerplate for project 8

--- 07-slider/src/data.js
+++ 07-slider/src/data.js
@@ -3,7 +3,7 @@ const people = [
     id: 1,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-    name: '1 maria ferguson',
+    name: 'maria ferguson',
     title: 'office manager',
     quote:
       'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
@@ -12,7 +12,7 @@ const people = [
     id: 2,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    name: '2 john doe',
+    name: 'john doe',
     title: 'regular guy',
     quote:
       'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
@@ -21,7 +21,7 @@ const people = [
     id: 3,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
-    name: '3 peter smith',
+    name: 'peter smith',
     title: 'product designer',
     quote:
       'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
@@ -30,7 +30,7 @@ const people = [
     id: 4,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    name: '4 susan andersen',
+    name: 'susan andersen',
     title: 'the boss',
     quote:
       'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
