019d4a695103812bfad86326d8f7248d64ca0a49
whatthefoobar <irina.mntn@gmail.com>
2022-01-04 18:40:06 +0100

    so far so good

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -3,7 +3,43 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>;
+  const [name, setNames] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit} action="">
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => {
+              setNames(e.target.value);
+            }}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
